534039a8d06b817a7fcdaec58269e74e82ed8a15
Tetiana2309 <126595700+Tetiana2309@users.noreply.github.com>
2023-07-22 19:32:59 +0200

    home work
    fixes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,54 @@
 import { galleryItems } from "./gallery-items.js";
 
-const galleryList = document.querySelector(".gallery");
+const containerGalleryItems = document.querySelector(".gallery");
 
-const createGalleryItem = ({ preview, original, description }) => `
-  <li class="gallery__item">
-    <a class="gallery__link" href="${original}" data-original-img=${original}>
-      <img class="gallery__image" src="${preview}" alt="${description}" />
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
     </a>
-  </li>
-`;
+  </li>`
+  )
+  .join("");
 
-galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+containerGalleryItems.insertAdjacentHTML("beforeend", createGalleryMarkup);
 
-const instances = [];
-
-galleryList.addEventListener("click", (event) => {
-  event.preventDefault();
-  const original = event.target
-    .closest(".gallery__link")
-    .getAttribute("data-original-img");
-
-  const instance = basicLightbox.create(`
-    <img src="${original}" width="800" height="600">
-  `);
-  instances.push(instance);
-  instance.show();
-  document.addEventListener("keydown", (event) => onEscPress(event, instance));
+const galleryImages = document.querySelectorAll(".gallery__image");
+galleryImages.forEach((image) => {
+  image.addEventListener("click", onImgClick);
 });
 
-const onEscPress = (event, instance) => {
-  const ESC_KEYCODE = "Escape";
-  if (event.code === ESC_KEYCODE) {
-    instance.close();
-    instances.splice(instances.indexOf(instance), 1);
-    document.removeEventListener("keydown", (event) =>
-      onEscPress(event, instance)
+function onhandleEscapeKeydown(instance) {
+  return function (event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+}
+
+function onImgClick(event) {
+  event.preventDefault();
+  const clickedElement = event.target;
+
+  if (clickedElement.classList.contains("gallery__image")) {
+    const instance = basicLightbox.create(
+      `<img src="${clickedElement.dataset.source}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", handleEscapeKeydown);
+        },
+        onClose: (instance) => {
+          document.removeEventListener("keydown", handleEscapeKeydown);
+        },
+      }
     );
+    const handleEscapeKeydown = onhandleEscapeKeydown(instance);
+    instance.show();
   }
-};
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery");
+const containerGalleryItems = document.querySelector(".gallery");
 
-galleryItems.forEach(({ preview, original, description }) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
+const createGalleryMarkyp = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+containerGalleryItems.insertAdjacentHTML("beforeend", createGalleryMarkyp);
 
-  const link = document.createElement("a");
-  link.classList.add("gallery__link");
-  link.href = original;
-
-  const image = document.createElement("img");
-  image.classList.add("gallery__image");
-  image.src = preview;
-  image.alt = description;
-
-  link.append(image);
-  galleryItem.append(link);
-  galleryList.append(galleryItem);
-});
-
-const lightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
-
-console.log(galleryItems);
